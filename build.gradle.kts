4a99d965ef320aede254b83a1ba37ad2d1d513b2
Hummel009 <hummel.turbamentum@gmail.com>
2023-11-10 16:23:17 +0300

    1

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import net.minecraftforge.gradle.user.UserExtension
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+buildscript {
+	repositories {
+		mavenCentral()
+		maven {
+			name = "forge"
+			url = uri("https://maven.minecraftforge.net/")
+		}
+	}
+	dependencies {
+		classpath("com.anatawa12.forge:ForgeGradle:1.2-1.1.+") {
+			isChanging = true
+		}
+	}
+}
+
+apply(plugin = "forge")
+
+plugins {
+	id("java")
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(8)
+	}
+	sourceCompatibility = JavaVersion.VERSION_1_8
+	targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+group = "org.example"
+version = "v" + LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
+
+val Project.minecraft: UserExtension
+	get() = extensions.getByName<UserExtension>("minecraft")
+
+minecraft.version = "1.7.10-10.13.4.1614-1.7.10"
+
+configure<UserExtension> {
+	replacements["@version@"] = project.version
+	runDir = "run"
+}
+
+val embed: Configuration by configurations.creating
+
+dependencies {
+	embed("net.java.dev.jna:jna:5.13.0")
+	implementation("net.java.dev.jna:jna:5.13.0")
+}
+
+tasks {
+	jar {
+		from(embed.map {
+			if (it.isDirectory) it else zipTree(it)
+		})
+		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+	}
+	withType<JavaCompile> {
+		options.encoding = "UTF-8"
+	}
+}
