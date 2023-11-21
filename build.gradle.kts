09dcaae5f4d43ac3052ecde0c140371c01a2db67
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-11-21 12:56:52 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,54 +1,53 @@
-import net.minecraftforge.gradle.user.UserExtension
+import org.jetbrains.gradle.ext.Gradle
+import org.jetbrains.gradle.ext.RunConfigurationContainer
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-buildscript {
-	repositories {
-		mavenCentral()
-		maven {
-			name = "forge"
-			url = uri("https://maven.minecraftforge.net/")
-		}
-	}
-	dependencies {
-		classpath("com.anatawa12.forge:ForgeGradle:1.2-1.1.+") {
-			isChanging = true
-		}
-	}
+plugins {
+	id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.7"
+	id("com.gtnewhorizons.retrofuturagradle") version "1.3.24"
 }
 
-apply(plugin = "forge")
+group = "hummel"
+version = "v" + LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 
-plugins {
-	id("java")
+dependencies {
+	embed("net.java.dev.jna:jna:5.13.0")
+	implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+	implementation("net.java.dev.jna:jna:5.13.0")
 }
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(8)
+		languageVersion.set(JavaLanguageVersion.of(8))
 	}
-	sourceCompatibility = JavaVersion.VERSION_1_8
-	targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-group = "org.example"
-version = "v" + LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
-
-val Project.minecraft: UserExtension
-	get() = extensions.getByName<UserExtension>("minecraft")
-
-minecraft.version = "1.7.10-10.13.4.1614-1.7.10"
-
-configure<UserExtension> {
-	replacements["@version@"] = project.version
-	runDir = "run"
+minecraft {
+	mcVersion.set("1.7.10")
+	username.set("Hummel009")
 }
 
-val embed: Configuration by configurations.creating
-
-dependencies {
-	embed("net.java.dev.jna:jna:5.13.0")
-	implementation("net.java.dev.jna:jna:5.13.0")
+idea {
+	project {
+		this.withGroovyBuilder {
+			"settings" {
+				"runConfigurations" {
+					val self = this.delegate as RunConfigurationContainer
+					self.add(
+						Gradle("1. Run Client").apply {
+							setProperty("taskNames", listOf("runClient"))
+						},
+					)
+					self.add(
+						Gradle("2. Run Server").apply {
+							setProperty("taskNames", listOf("runServer"))
+						},
+					)
+				}
+			}
+		}
+	}
 }
 
 tasks {
@@ -58,7 +57,7 @@ tasks {
 		})
 		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 	}
-	withType<JavaCompile> {
+	withType<JavaCompile>().configureEach {
 		options.encoding = "UTF-8"
 	}
 }
