26546a4b69d4e84335e35a18fe98ba23cd9becb1
Hummel009 <hummel.turbamentum@gmail.com>
2024-09-11 12:15:02 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ val embed: Configuration by configurations.creating
 dependencies {
 	embed("net.java.dev.jna:jna:latest.release")
 	implementation("net.java.dev.jna:jna:latest.release")
+	implementation(rfg.deobf(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar")))))
 }
 
 java {
