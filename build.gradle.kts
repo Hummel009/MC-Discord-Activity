92db9a28c5489c003e02c3119f801c284dc324c5
Hummel009 <hummel.turbamentum@gmail.com>
2024-11-02 14:33:51 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,11 +28,25 @@ minecraft {
 }
 
 tasks {
+	register("copyForgeUnpacked") {
+		doLast {
+			val userHome = System.getProperty("user.home")
+			val gradleUserHome = System.getenv("GRADLE_USER_HOME") ?: "$userHome/.gradle"
+			val sourceDir = File("$gradleUserHome/caches/minecraft/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10")
+			val targetDir =
+				File("$userHome/.gradle/caches/minecraft/net/minecraftforge/forge/1.7.10-10.13.4.1614-1.7.10")
+			targetDir.mkdirs()
+			sourceDir.copyRecursively(targetDir, overwrite = false)
+		}
+	}
 	jar {
 		from(embed.map {
 			if (it.isDirectory) it else zipTree(it)
 		})
 		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+	}
+	runClient {
+		dependsOn("copyForgeUnpacked")
 	}
 	withType<JavaCompile>().configureEach {
 		options.encoding = "UTF-8"
